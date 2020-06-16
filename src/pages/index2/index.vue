e7e08ce4306c13aaf8e8a1c50967d197e0e8dd00
zhuliheng <1074936307@qq.com>
2020-06-16 18:16:26 +0800

    新改动

---
+++ src/pages/index2/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="content">
+		<view class="rpx">rpx</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+	
+	}
+</script>
+
+<style>
+	.rpx{
+		height: 750rpx;
+		width: 100px;
+		background-color: aqua;
+	}
+</style>
